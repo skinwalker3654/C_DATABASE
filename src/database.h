9fce8ac3f39d20e8d0b6f8cc1216bf02dbdb25f3
skinwalker <skinrr932@gmail.com>
2025-09-04 17:44:24 +0300

    Update database.h

--- src/database.h
+++ src/database.h
@@ -12,11 +12,13 @@ typedef struct Student_list {
     struct Student_list *next;
 } Student_list;
 
+/*UTIL FUNCTIONS*/
 void save_students_to_file(Student_list *studentPtr);
 void load_students_from_file(Student_list **studentPtr);
 void sort_students_by_id(Student_list *studentPtr);
 void free_list_nodes(Student_list *studentPtr);
 
+/*MAIN FUNCTIONS*/
 void add_student_to_list(Student_list **studentPtr,int id,char *name,float grade);
 void delete_student_from_list(Student_list **studentPtr,int id);
 void print_student_list(Student_list *studentPtr);
