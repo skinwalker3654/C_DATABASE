6026d58669701240e579f9ee7f47a02c3ea58081
skinwalker <skinrr932@gmail.com>
2025-09-19 23:15:53 +0300

    Update utils.h

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -14,7 +14,7 @@ void free_list_nodes(Student_list *studentPtr);
 void select_operations(Student_list **nodes,char *operation,float number);
 void delete_operations(Student_list **nodes,char *operation,float number);
 
-/*HELP PANELS*/
+/*HELP PANNELS*/
 void help_show_select_command(void);
 void help_show_delete_command(void);
 void help_show_commands(void);
